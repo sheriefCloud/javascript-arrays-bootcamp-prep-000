fc0c493cf86216be9d42dd2f181b46d6381d3170
Sherief Farid <sherief@farid.cloud>
2019-02-03 00:50:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,10 @@
-// var chocolateBars = [`snikers`,`hundred grand`,`kitkat`,`skittles`];
+
 
 var chocolateBars = new Array ("snikers","hundred grand","kitkat","skittles");
+
+
+var addElementToBeginningOfArray = function (array1,elementAdd) {
+  array1.unshift (elementAdd)
+  return array1
+}
+
